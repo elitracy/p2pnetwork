615ea866f8c7874e6820c32f161907340bf34859
Elias Tracy <elitracy19@gmail.com>
2025-07-14 17:28:52 -0500

    feat: added connected field

--- shared/device.go
+++ shared/device.go
@@ -6,11 +6,12 @@ import (
 )
 
 type Device struct {
-	ID       uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
-	Name     string    `gorm:"not null"`
-	PubKey   string    `gorm:"not null;uniqueIndex"`
-	IP       string    `gorm:"type:inet;not null"`
-	Port     string    `gorm:"not null"`
-	Endpoint string    `gorm:"not null"`
-	LastSeen time.Time `gorm:"not null"`
+	ID        uuid.UUID `gorm:"type:uuid;default:gen_random_uuid();primaryKey"`
+	Name      string    `gorm:"not null"`
+	PubKey    string    `gorm:"not null;uniqueIndex"`
+	IP        string    `gorm:"type:inet;not null"`
+	Port      string    `gorm:"not null"`
+	Endpoint  string    `gorm:"not null"`
+	LastSeen  time.Time `gorm:"not null"`
+	Connected bool      `gorm:"not null"`
 }
